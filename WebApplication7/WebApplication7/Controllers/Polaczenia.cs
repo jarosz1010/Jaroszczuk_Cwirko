6f01d1decc3222913eb3b6178fc5545ad36ca366
patryk <sdark431@gmail.com>
2020-05-06 17:09:55 +0200

    Dodanie bootstrapa

--- WebApplication7/WebApplication7/Controllers/Polaczenia.cs
+++ WebApplication7/WebApplication7/Controllers/Polaczenia.cs
@@ -40,6 +40,7 @@ namespace WebApplication7.Controllers
         {
             return View();
         }
+
         // GET: Polaczenia/Details/5
         public async Task<IActionResult> Details(int? id)
         {
