07790eec89de10630ac652d67dd36533f500b96f
L3iMhC <47970204+L3iMhC@users.noreply.github.com>
2020-05-12 00:35:40 +0200

    Działajace i pozytywnie przeprowadzone testy

--- WebApplication7/WebApplication7.Tests/Tests/Controllers/Pracownicy_Tests.cs
+++ WebApplication7/WebApplication7.Tests/Tests/Controllers/Pracownicy_Tests.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.Extensions.DependencyInjection;
 using WebApplication7;
 using WebApplication7.Controllers;
 using WebApplication7.Models;
@@ -19,39 +21,43 @@ namespace WebApplication7.Tests
 {
     public class Pracownicy_tests
     {
+
         [Fact]
         public void Index_Test()
         {
-            var mockrepo1 = new Mock<WebApplication7Context>() ;//Brak bezparametrycznego konstruktora - dostepny konstruktor nie moze byc bezposrednio uzywany
+            var builder = new DbContextOptionsBuilder<WebApplication7Context>().UseInMemoryDatabase("WebApplication7");
+            var context = new WebApplication7Context(builder.Options);
+
+            // var mockrepo1 = new Mock<WebApplication7Context>() ;//Brak bezparametrycznego konstruktora - dostepny konstruktor nie moze byc bezposrednio uzywany
             var mockrepo2 = new Mock<ILogger<Pracownicy>>();
             //  mockrepo1.Object.Pracownik.ToListAsync();
 
-            var controller = new Pracownicy(mockrepo1.Object, mockrepo2.Object);
+            var controller = new Pracownicy(context, mockrepo2.Object);
 
-            var result = controller.Index();
+            var result = controller.Index().Result;
 
             Assert.IsType<ViewResult>(result);
         }
-        
+ 
        
-      /*
+      
         
         [Fact]
         public void Zaloguj_Admin_Tests()
         {
-            var mockrepo1 = new Mock<WebApplication7Context>();
+            var builder = new DbContextOptionsBuilder<WebApplication7Context>().UseInMemoryDatabase("WebApplication7");
+            var context = new WebApplication7Context(builder.Options);
             var mockrepo2 = new Mock<ILogger<Pracownicy>>();
-            var controller = new Pracownicy(mockrepo1.Object, mockrepo2.Object);
+            var controller = new Pracownicy(context, mockrepo2.Object);
             string login, haslo;
             login = "kasjer1"; haslo = "kasjer1";
 
-            var result = controller.Zaloguj_admin(login, haslo);
+            var result = controller.Zaloguj_admin(login, haslo).Result as ViewResult;
 
 
 
-            Assert.Equal<>
-
+            Assert.IsType<ViewResult>(result);
+            
         }
-        */
     }
 }
